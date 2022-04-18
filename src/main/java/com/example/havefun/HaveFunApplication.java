cbb0de79294da10344e7389eab87cdd5b1546e8c
YuanChunhai <YuanChunhai>
2022-04-18 10:24:40 +0800

    init

---
+++ src/main/java/com/example/havefun/HaveFunApplication.java
@@ -0,0 +1,13 @@
+package com.example.havefun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaveFunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaveFunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/havefun/HaveFunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.havefun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaveFunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
